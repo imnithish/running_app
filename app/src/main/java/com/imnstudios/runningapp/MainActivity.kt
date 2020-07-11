e08db6eef8ac4a9fa682792d41da962ed67a5c12
imnithish <imnithish@live.com>
2020-07-11 17:03:22 +0530

    initial commit

---
+++ app/src/main/java/com/imnstudios/runningapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.imnstudios.runningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
