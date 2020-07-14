4f805b2526fe9166e95c5d518b7f43dea18d3580
imnithish <imnithish@live.com>
2020-07-14 14:40:05 +0530

    Database class

---
+++ app/src/main/java/com/imnstudios/runningapp/db/RunningDatabase.kt
@@ -0,0 +1,15 @@
+package com.imnstudios.runningapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDao
+}
