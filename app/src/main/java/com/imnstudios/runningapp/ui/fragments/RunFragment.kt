30ce0bcfd6c328fddd7f0d5cf8b503fb9cb5c84c
Nitheesh AG <nitheesh@riafy.me>
2020-07-20 00:04:42 +0530

    navigation components are awesome

--- app/src/main/java/com/imnstudios/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/imnstudios/runningapp/ui/fragments/RunFragment.kt
@@ -1,13 +1,24 @@
 package com.imnstudios.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.imnstudios.runningapp.R
 import com.imnstudios.runningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment: Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
