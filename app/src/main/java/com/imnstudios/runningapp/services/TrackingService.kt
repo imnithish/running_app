041fb68954112dc09a668c1a0131f02ff700facc
Nitheesh AG <nitheesh@riafy.me>
2020-07-24 01:26:53 +0530

    TrackingService implemented

---
+++ app/src/main/java/com/imnstudios/runningapp/services/TrackingService.kt
@@ -0,0 +1,31 @@
+package com.imnstudios.runningapp.services
+
+import android.content.Intent
+import androidx.lifecycle.LifecycleService
+import com.imnstudios.runningapp.other.Constants.ACTION_PAUSE_SERVICE
+import com.imnstudios.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.imnstudios.runningapp.other.Constants.ACTION_STOP_SERVICE
+import timber.log.Timber
+
+class TrackingService : LifecycleService() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        intent?.let {
+            when(it.action){
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    Timber.d("Started or resumed service")
+                }
+                ACTION_PAUSE_SERVICE -> {
+                    Timber.d("Paused service")
+                }
+                ACTION_STOP_SERVICE -> {
+                    Timber.d("Stopped service")
+                }
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+
+    }
+}
--- app/src/main/java/com/imnstudios/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/imnstudios/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,72 @@
 package com.imnstudios.runningapp.ui.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.imnstudios.runningapp.R
+import com.imnstudios.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.imnstudios.runningapp.services.TrackingService
 import com.imnstudios.runningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 
 @AndroidEntryPoint
-class TrackingFragment: Fragment(R.layout.fragment_tracking) {
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+
+        btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
