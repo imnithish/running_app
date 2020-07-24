59e1f10b909fb2809b76e72da1d7dcde9b35587a
Nitheesh AG <nitheesh@riafy.me>
2020-07-24 21:40:26 +0530

    Tracking user location in background

--- app/src/main/java/com/imnstudios/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/imnstudios/runningapp/ui/MainActivity.kt
@@ -17,6 +17,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
